aac11284523f1da077feee90a6b4104a0c02fdb3
Mahmud Al Hakim <mahmud.alhakim@gmail.com>
2023-03-15 13:23:27 +0100

    JS Exempel

---
+++ Lektion04/events.js
@@ -0,0 +1,11 @@
+function display(){
+    console.log("Hello Events!");
+}
+
+function print(){
+    console.log("Hello Events 2");
+}
+
+function getTime(){
+    document.getElementById('time').innerHTML = new Date().toLocaleTimeString();
+}
